4bbfa4deb7878e6fd7bb8048d4cbbc1e852a4b93
Nkennyelvis <e.nsengiman@alustudent.com>
2025-08-03 16:03:24 +0200

    api adv

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -3,6 +3,7 @@
 This module contains a function that queries the Reddit API and
  returns the number of subscribers for a given subreddit.
 """
+
 import requests
 
 
@@ -34,4 +35,3 @@ if __name__ == "__main__":
         print("Please pass an argument for the subreddit to search.")
     else:
         print("{:d}".format(number_of_subscribers(sys.argv[1])))
-        
